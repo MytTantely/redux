2e102f89334ad4b6776bc7cd908379d72f31b44f
Tantely Maharisoa <tantelymaharisoa@Tantelys-MacBook-Pro.local>
2018-12-08 08:26:08 -0700

    lesson 7

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
+// 5:42
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import { createStore } from 'redux';
+
+function reducer(state, action){
+    if(action.type === 'changeState'){
+        return action.payload;
+    }
+
+    return 'State';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+    type: 'changeState',
+    payload: {
+        newState: 'New State'
+    }
+}
+ 
+store.dispatch(action);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
